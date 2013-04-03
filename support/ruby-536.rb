d258ccfe63dc16388dd9360ffa4d544a1a3f21c8
Tyler Brock <tyler.brock@gmail.com>
2013-04-03 11:45:15 -0400

    ruby-536 solution

--- support/ruby-536.rb
+++ support/ruby-536.rb
@@ -1,14 +1,18 @@
 #!/usr/bin/env ruby
 require 'mongo'
+require 'base64'
 include Mongo
 
-def test_binary_2
-  line = 'db.users.remove();db.users.insert({name: "Jim", someBinData: new BinData(2,"1234")});'
-  File.open('/tmp/bindata.js', 'w'){|f| f.write(line)}
+def test_binary
+  data = Base64.encode64([5].pack("l<") + "tyler").chop
+  lines = [
+    "db.users.drop();",
+    "db.users.insert({data: new BinData(2,'#{data}')});"
+  ]
+  File.open('/tmp/bindata.js', 'w'){|f| f.puts(lines)}
   system('mongo /tmp/bindata.js > /dev/null 2>&1')
   coll = Mongo::MongoClient.new('localhost', 27017)['test']['users']
-  p coll.find.first['someBinData'].to_s
-  puts "done"
+  p coll.find.first['data'].to_s
 end
 
-test_binary_2
+test_binary
